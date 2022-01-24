3af4213628cffcffeeb4880d150b3d24ce9ff420
nagoy <nagoymail@gmail.com>
2022-01-24 22:27:43 +0500

    юнит 3 в доделан

--- unit_03/unit_03/unit_03.js
+++ unit_03/unit_03/unit_03.js
@@ -5,7 +5,9 @@
 let a1 = ['hello', 't'];
 
 const f1 = () => {
-
+    let in1 = document.querySelector('.i-1').value;
+    a1.push(in1);
+    console.log(a1);
 }
 
 // TASK 02
@@ -14,7 +16,9 @@ const f1 = () => {
 let a2 = ['b', 'c', 12, 34, 'dh', 17];
 
 const f2 = () => {
-
+    let in2 = document.querySelector('.i-2').value;
+    a2.push(in2, in2 ** 3);
+    console.log(a2);
 }
 
 // TASK 03
@@ -24,9 +28,9 @@ const f2 = () => {
 let a3 = ['a', 'b'];
 
 const f3 = () => {
-    // ваш код
-    // console.log(a3.push(ваша переменная));
-    // console.log(a3);
+    let in3 = document.querySelector('.i-3').value; // ваш код
+    console.log(a3.push(in3)); // console.log(a3.push(ваша переменная));
+    console.log(a3); // console.log(a3);
 }
 
 // TASK 04
@@ -34,10 +38,11 @@ const f3 = () => {
 
 
 let a4 = [1, 2];
-let a4_1 = [5,6];
+let a4_1 = [5, 6];
 
 const f4 = () => {
-
+    a4.push(a4_1);
+    console.log(a4);
 }
 
 // TASK 05
@@ -47,7 +52,9 @@ const f4 = () => {
 let a5 = [55, 66, 77];
 
 const f5 = () => {
-
+    let in5 = +document.querySelector('.i-5').value;
+    a5[a5.length] = in5;
+    console.log(a5);
 }
 
 // TASK 06
@@ -57,7 +64,9 @@ const f5 = () => {
 let a6 = ['a', 'b'];
 
 const f6 = (arr, elem) => {
-
+    arr[arr.length] = elem;
+    console.log(arr);
+    return arr.length;
 }
 
 // TASK 07
@@ -66,7 +75,8 @@ const f6 = (arr, elem) => {
 let a7 = [21, 22, 23, 24, 25, 26, 27];
 
 const f7 = () => {
-
+    a7.pop();
+    console.log(a7);
 }
 
 // TASK 08
@@ -74,17 +84,27 @@ const f7 = () => {
 // Нажимая кнопку удалите все элементы. Изучите что возвращает операция когда есть элементы доступные для удаления и когда массив пуст.
 
 
-let a8 = [99,98,97,96];
+let a8 = [99, 98, 97, 96];
 
 const f8 = () => {
+    return a8.pop();
 }
 
 // TASK 09
 // По нажатию b-9 выполняется функция f9. Функция должна применить pop() к массиву a9. После применения массив выведите в консоль.
 
-let a9 = ['A', 2, true, function() {console.log(2)}, [2,3,4], {"test": 1}];
+let a9 = ['A', 2, true, function () {
+        console.log(2);
+    },
+    [2, 3, 4], {
+        "test": 1
+    }
+];
 
 const f9 = () => {
+    console.log(a9);
+    a9.pop();
+    console.log(a9);
 }
 
 // TASK 10
@@ -93,7 +113,12 @@ const f9 = () => {
 const a10 = [67, '55', 2, 5, '4', '8', 8, '66', '54', 11];
 
 const f10 = () => {
-
+    if (a10.length === 0) return undefined;
+    else {
+        let tmp = a10[a10.length - 1];
+        a10.length = a10.length - 1;
+        return tmp;
+    }
 }
 
 
@@ -102,15 +127,15 @@ document.querySelector('.b-2').addEventListener('click', f2);
 document.querySelector('.b-3').addEventListener('click', f3);
 document.querySelector('.b-4').addEventListener('click', f4);
 document.querySelector('.b-5').addEventListener('click', f5);
-document.querySelector('.b-6').addEventListener('click', ()=>{
+document.querySelector('.b-6').addEventListener('click', () => {
     let elem = document.querySelector('.i-6').value;
     console.log(f6(a6, elem));
 });
 document.querySelector('.b-7').addEventListener('click', f7);
-document.querySelector('.b-8').addEventListener('click', ()=>{
+document.querySelector('.b-8').addEventListener('click', () => {
     console.log(f8());
 });
 document.querySelector('.b-9').addEventListener('click', f9);
-document.querySelector('.b-10').addEventListener('click', ()=>{
+document.querySelector('.b-10').addEventListener('click', () => {
     console.log(f10());
 });
